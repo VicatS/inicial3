047bce06b85d007dbae5bf994834534618c4bc2b
vicco <victorcs2704@gmail.com>
2020-04-28 21:52:00 -0400

    05 Crear controlador para los roles y la vista index

---
+++ routes/comandosUtilizados.php
@@ -0,0 +1,18 @@
+<?php
+
+Route::get('/test', function () {
+
+//    return Permission::create([
+//        'name' => 'Listado de Permisos',
+//        'slug' => 'listado-de-permisos',
+//        'description' => 'Ver listado de Permisos'
+//    ]);
+
+//    return Permission::all();
+//    $role = Role::find(1);
+//
+//    $role->permissions()->sync([1,2,3,4]);
+//
+//    return $role->permissions;
+
+});
--- routes/web.php
+++ routes/web.php
@@ -24,19 +24,9 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/test', function () {
-
-//    return Permission::create([
-//        'name' => 'Listado de Permisos',
-//        'slug' => 'listado-de-permisos',
-//        'description' => 'Ver listado de Permisos'
-//    ]);
-
-//    return Permission::all();
-//    $role = Role::find(1);
-//
-//    $role->permissions()->sync([1,2,3,4]);
-//
-//    return $role->permissions;
-
+Route::group(['namespace' => 'Security'], function () {
+    Route::resource('/role', 'RoleController')->names('role');
 });
+
+
+
