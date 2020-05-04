7fd74e9e377279cfbf19ee61fd2d5a9e59c40797
vicco <victorcs2704@gmail.com>
2020-05-04 19:46:37 -0400

    09. Metodos edit y update

--- app/Http/Controllers/Security/RoleController.php
+++ app/Http/Controllers/Security/RoleController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class RoleController extends Controller
 {
-    /**
+    /*
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -21,7 +21,7 @@ class RoleController extends Controller
         return view('role.index', compact('roles'));
     }
 
-    /**
+    /*
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
@@ -33,7 +33,7 @@ class RoleController extends Controller
         return view('role.create', compact('permissions'));
     }
 
-    /**
+    /*
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -42,9 +42,9 @@ class RoleController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-           'name' => 'required|max:50|unique:roles,name',
-           'slug' => 'required|max:50|unique:roles,slug',
-           'full_access' => 'required|in:yes,no'
+            'name' => 'required|max:50|unique:roles,name',
+            'slug' => 'required|max:50|unique:roles,slug',
+            'full_access' => 'required|in:yes,no'
         ]);
 
         $role = Role::create($request->all());
@@ -58,7 +58,7 @@ class RoleController extends Controller
             ->with('status_success', 'Role saved successfully');
     }
 
-    /**
+    /*
      * Display the specified resource.
      *
      * @param  int  $id
@@ -69,30 +69,52 @@ class RoleController extends Controller
         //
     }
 
-    /**
+    /*
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Role $role)
     {
-        //
+        $permission_role = [];
+
+        foreach ($role->permissions as $permission) {
+            $permission_role[] = $permission->id;
+        }
+
+        $permissions = Permission::get();
+
+        return view('role.edit', compact('permissions', 'role', 'permission_role'));
     }
 
-    /**
+    /*
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Role $role)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:50|unique:roles,name,' . $role->id,
+            'slug' => 'required|max:50|unique:roles,slug,' . $role->id,
+            'full_access' => 'required|in:yes,no'
+        ]);
+
+        $role->update($request->all());
+
+        $permission = $request->get('permission');
+        if ($permission) {
+            $role->permissions()->sync($permission);
+        }
+
+        return redirect()->route('role.index')
+            ->with('status_success', 'Role updated successfully');
     }
 
-    /**
+    /*
      * Remove the specified resource from storage.
      *
      * @param  int  $id
---
+++ resources/views/role/edit.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Edit Role</div>
+
+                    <div class="card-body">
+                        @include('custom.message')
+
+                        <form action="{{ route('role.update', $role->id) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="container">
+                                <h3>Required data</h3>
+
+                                <div class="form-group">
+                                    <input type="text" class="form-control"
+                                           placeholder="Name" name="name"
+                                           value="{{ old('name', $role->name) }}"
+                                    >
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="text" class="form-control"
+                                           placeholder="Slug" name="slug"
+                                           value="{{ old('slug', $role->slug) }}"
+                                    >
+                                </div>
+
+                                <div class="form-group">
+                                    <textarea class="form-control" rows="3" placeholder="Description"
+                                              name="description">
+                                        {{ old('description', $role->name) }}
+                                    </textarea>
+                                </div>
+                                <hr>
+                                <h3>Full Access</h3>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="full_access_yes" name="full_access"
+                                           class="custom-control-input" value="yes"
+                                           @if($role['full_access'] == "yes")
+                                           checked
+                                           @elseif(old('full_access') == "yes")
+                                           checked
+                                        @endif
+                                    >
+                                    <label class="custom-control-label" for="full_access_yes">Yes</label>
+                                </div>
+                                <div class="custom-control custom-radio custom-control-inline">
+                                    <input type="radio" id="full_access_no" name="full_access"
+                                           class="custom-control-input" value="no"
+                                           @if($role['full_access'] == "no")
+                                           checked
+                                           @elseif(old('full_access') == "no")
+                                           checked
+                                        @endif
+                                    >
+                                    <label class="custom-control-label" for="full_access_no">No</label>
+                                </div>
+                                <hr>
+
+                                <h3>Permission List</h3>
+
+                                @foreach($permissions as $key => $permission)
+                                    <div class="custom-control custom-checkbox">
+                                        <input type="checkbox" class="custom-control-input"
+                                               id="permission_{{ $permission->id }}"
+                                               name="permission[]" value="{{ $permission->id }}"
+                                               @if(is_array(old('permission')) && in_array("$permission->id", old('permission')))
+                                               checked
+                                               @elseif(is_array($permission_role) && in_array("$permission->id", $permission_role))
+                                               checked
+                                            @endif
+                                        >
+                                        <label class="custom-control-label" for="permission_{{ $permission->id }}">
+                                            {{ $permission->id }}
+                                            -
+                                            {{ $permission->name }}
+                                            <em>({{ $permission->description }})</em>
+                                        </label>
+                                    </div>
+                                @endforeach()
+                                <hr>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
